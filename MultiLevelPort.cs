fe07d0c9e646dfa3c5996e490e06d15fd1a80a02
PerepelitsunaEE <43075958+PerepelitsunaEE@users.noreply.github.com>
2018-11-29 21:32:09 +0400

    Add files via upload

---
+++ MultiLevelPort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsBoats
+{
+    class MultiLevelPort
+    {
+        /// <summary>
+        /// Список с уровнями порта
+        /// </summary>
+        List<Port<IBoat>> parkingStages;
+        /// <summary>
+        /// Сколько мест на каждом уровне
+        /// </summary>
+        private const int countPlaces = 20;
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="countStages">Количество уровенй порта</param>
+        /// <param name="pictureWidth"></param>
+        /// <param name="pictureHeight"></param>
+        public MultiLevelPort(int countStages, int pictureWidth, int pictureHeight)
+        {
+            parkingStages = new List<Port<IBoat>>();
+            for (int i = 0; i < countStages; ++i)
+            {
+                parkingStages.Add(new Port<IBoat>(countPlaces, pictureWidth, pictureHeight));
+            }
+        }
+        /// <summary>
+        /// Индексатор
+        /// </summary>
+        /// <param name="ind"></param>
+        /// <returns></returns>
+        public Port<IBoat> this[int ind]
+        {
+            get
+            {
+                if (ind > -1 && ind < parkingStages.Count)
+                {
+                    return parkingStages[ind];
+                }
+                return null;
+            }
+        }
+    }
+}
