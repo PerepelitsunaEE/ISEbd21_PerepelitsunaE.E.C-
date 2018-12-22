d02645978fa79ec011be47fe41d041923d4c5ed8
PerepelitsunaEE <43075958+PerepelitsunaEE@users.noreply.github.com>
2018-12-22 08:47:22 +0400

    Add files via upload

---
+++ IBoat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsBoats
+{
+    public interface IBoat
+    {
+        void SetPosition(int x, int y, int width, int height);
+        void MoveTransport(Direction direction);
+        void DrawBoat(Graphics g);
+    }
+}
